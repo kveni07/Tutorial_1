085ae637edae4f3aca5b5c41c30dde9128261bf0
kveni07 <kveni07@gmail.com>
2025-07-28 10:47:20 +0100

    HL_Updated_Code

--- Scripts/Haut_Lomami_Final.R
+++ Scripts/Haut_Lomami_Final.R
@@ -98,3 +98,7 @@ pre_ea_final <- pre_ea_final %>%
 st_write(pre_ea_final, paste0(output_path, "Haut_Lomami_combined.gpkg"), delete_layer = TRUE)
 
 
+#Export the result as csv
+st_write(pre_ea_final, paste0(output_path, "Haut_Lomami_combined.gpkg"), delete_layer = TRUE)
+
+
